cb121ebbb78d844eb56aa20ed7831025d1758386
victoria0506 <vicguti527@gmail.com>
2024-06-02 17:07:21 -0600

    funcion-botones

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,2 +1,47 @@
 // Inserte el código aquí
 
+let bntAgregar = document.getElementById("agregar")
+let input = document.getElementById("tarea")
+let lista = document.getElementById("lista")
+
+let contador = document.getElementById("cantidad")
+
+bntAgregar.addEventListener("click", function (){
+    if (input.value != "") {
+
+        let btnCheck = document.createElement("input")
+        btnCheck.type = "checkbox"
+        lista.appendChild(btnCheck)
+       
+        btnCheck.addEventListener("click", function() {
+            console.log("tarea lista");
+            contador.innerHTML++
+        })
+        
+        let parrafo = document.createElement("p")
+        lista.appendChild(parrafo).innerHTML = input.value
+
+    }else{
+        alert("Ingrese un texto")
+    }
+})
+
+
+input.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+
+        if (input.value != "") {
+
+            let btnCheck = document.createElement("input")
+            btnCheck.type = "checkbox"
+            lista.appendChild(btnCheck)
+            
+            let parrafo = document.createElement("p")
+            lista.appendChild(parrafo).innerHTML = input.value
+    
+        }else{
+            alert("Ingrese un texto")
+        }
+    }
+})
+
